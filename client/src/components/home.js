4d7301391e0a26eff0944779f5ddc9c3fcbb4427
Kerim Demir <kerim.demir2002@hotmail.com>
2023-01-27 18:55:08 +0300

    Fix: prop value doesn't change after input entered.

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -3,69 +3,58 @@ import {useState, useEffect} from "react"
 
 
 const Row = (props) => {
-    const [state, setState] = useState(props);
+    const [state, setState] = useState(props.state);
+
+    
+    // const [letter1, setLetter1] = useState("");
+    // const [letter2, setLetter2] = useState("");
+    // const [letter3, setLetter3] = useState("");
+    // const [letter4, setLetter4] = useState("");
+    // const [letter5, setLetter5] = useState("");
+    
+    const currentWord = state["guess" + state.count];
     
     useEffect(() => {
-      setState(props);
-    }, [props])
-    
-    const [letter1, setLetter1] = useState("");
-    const [letter2, setLetter2] = useState("");
-    const [letter3, setLetter3] = useState("");
-    const [letter4, setLetter4] = useState("");
-    const [letter5, setLetter5] = useState("");
+      setState(props.state);
+    }, [props.state])
 
-    const handleChange = (e,id) => {
-        const newGuess = guess + e.target.value;
-        // setGuess(newGuess);
-        if(id===1)
-            setLetter1(e.target.value.toUpperCase());
-        else if(id===2)
-            setLetter2(e.target.value.toUpperCase());
-        else if(id===3)
-            setLetter3(e.target.value.toUpperCase());
-        else if(id===4)
-            setLetter4(e.target.value.toUpperCase());
-        else if(id===5)
-            setLetter5(e.target.value.toUpperCase());
-
-    }
-
-    const handleSubmit = () => {
-
-    }
-
-    
     return (
         <div>
-            <input id="1" name="guess" value={letter1} onChange={(e)=> handleChange(e,1)} maxLength={1} className="letter-block"/>
-            <input id="2" name="guess" value={letter2} onChange={(e)=> handleChange(e,2)} maxLength={1} className="letter-block"/>
-            <input id="3" name="guess" value={letter3} onChange={(e)=> handleChange(e,3)} maxLength={1} className="letter-block"/>
-            <input id="4" name="guess" value={letter4} onChange={(e)=> handleChange(e,4)} maxLength={1} className="letter-block"/>
-            <input id="5" name="guess" value={letter5} onChange={(e)=> handleChange(e,5)} maxLength={1} className="letter-block"/>
+            <input value={currentWord[0]} maxLength={1} className="letter-block"/>
+            <input value={currentWord[1]} maxLength={1} className="letter-block"/>
+            <input value={currentWord[2]} maxLength={1} className="letter-block"/>
+            <input value={currentWord[3]} maxLength={1} className="letter-block"/>
+            <input value={currentWord[4]} maxLength={1} className="letter-block"/>
         </div>
     )
-}
+};
 
-const HiddenInput = (guess) => {
-    const [newGuess, setNewGuess] = useState(guess);
+const HiddenInput = (props) => {
+    const [state, setState] = useState(props.state);
 
     const handleChange = (e) => {
-        setNewGuess(e.target.value);
+        setState({...state, ["guess" + state.count]: e.target.value})
     }
+
+    const handleSubmit= () => {
+        setState({...state, count: state.count+1});
+    }
+
     return (
-        <input autoFocus maxLength={5} value={newGuess} onChange={handleChange}/>
+        <form  onSubmit={handleSubmit}>
+            <input autoFocus maxLength={5} value={state["guess" + state.count]} onChange={handleChange}/>
+        </form>
         )
-    }
+    };
 
 const Finish = () => {
     //game is over word is not found
-}
+};
 
 const Home = () => {
     const word = <RandomWord/>;
     
-    const [state, setState] = {
+    const [state, setState] = useState({
         guess1: "",
         guess2: "",
         guess3: "",
@@ -73,8 +62,13 @@ const Home = () => {
         guess5: "",
         guess6: "",
         count: 1,
-        word: word,
-    }
+        word: word
+    })
+
+    useEffect(() => {
+        setState(state)
+    }, [state])
+    
 
     return (
         <>
@@ -82,16 +76,16 @@ const Home = () => {
             Wordle
         </div>
         <div style={{display: "block", margin: "auto", padding: "auto", textAlign: "center", marginTop: "50px", paddingTop: "50px"}}>
-             <Row {...state}/>
-             <Row {...state}/>
-             <Row {...state}/>
-             <Row {...state}/>
-             <Row {...state}/>
-             <Row {...state}/>
-            <HiddenInput {...state}/>
+             <Row state={state}/>
+             <Row state={state}/>
+             <Row state={state}/>
+             <Row state={state}/>
+             <Row state={state}/>
+             <Row state={state}/>
+            <HiddenInput state={state}/>
         </div>
         </>
     )
-}
+};
 
 export default Home;
